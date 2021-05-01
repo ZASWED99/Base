76b98f31487ba5cf897991c4a6c2e6a96c52aba9
Roman <shabunin.roman5@gmail.com>
2021-05-01 14:50:35 +0300

    first commit

---
+++ Variables/js/script.js
@@ -0,0 +1,76 @@
+"use strict";
+
+let nUmber = 6;
+const leftBorderWidth = 1;
+
+nUmber = 10;
+
+console.log(nUmber);
+
+const obj = {
+    a: 50
+};
+obj.a = 10;
+console.log(obj);
+
+let number = 4.6;
+console.log(-4 / 0);
+console.log('string' * 9);
+
+const persone = 'Alex kasjdklasdjk 5';
+
+const bool = true || false;
+
+let und;
+console.log(und);
+
+const object = {
+     name: 'John',
+     age: 25,
+     isMarried: false,
+};
+
+console.log(object.name);
+console.log(object['age']);
+
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[2]);
+
+
+
+
+alert('Hello');
+
+const result = confirm('Are you here?');
+
+const answer = prompt('Are you 18?', '18');
+
+const answers = [];
+answers[0] = prompt('Name?', '');
+answers[1] = prompt('Lastname?', '');
+answers[2] = prompt('Age', '');
+
+console.log(typeof(answers));
+console.log(typeof(null));
+
+
+const category = 'toys';
+console.log('https://someurl.com/' + category + '/' + '4');
+console.log(`https://someurl.com/${category}/5`);
+
+const user = 'Roman';
+alert(`Hello, ${user}`);
+
+
+console.log('arr' + ' - object');
+console.log(4 + +'5'):
+
+let incr = 10,
+    decr = 10;
+
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(++incr);
+console.log(--decr);
